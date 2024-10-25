0316d768b1a8060a575961554ee2540d4dd4dc56
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2024-10-25 22:45:23 +0530

    add: gh-pages config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
  
 export default defineConfig({
   plugins: [react()],
+  base: "/Aryan-Garg-dev/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
