07b062c175b1e035c9b2886b5de48d42b8e775c3
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2024-10-25 22:52:54 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vite"
  
 export default defineConfig({
   plugins: [react()],
-  base: "/Aryan-Garg-dev/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
