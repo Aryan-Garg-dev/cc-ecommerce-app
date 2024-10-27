a2f5d39f28807ddaa3b16167a2dea2d0821eb9df
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2024-10-27 11:53:53 +0530

    fix: gaping-hero-section

--- src/pages/home/sections/hero.tsx
+++ src/pages/home/sections/hero.tsx
@@ -7,12 +7,12 @@ export default function HeroSection(){
   return (
     <section className="w-full border bg-purple-50 shadow-md shadow-purple-100">
       <Banner {...bannerText} />
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center max-sm:mt-2">
         <TypingAnimation
           className="text-8xl max-md:text-6xl font-bebas font-bold text-[#17153B] mt-2 tracking-wide"
           text="Shop Karo"
         />
-        <div className="my-2">
+        <div className="flex flex-col max-sm:gap-2 mt-1 mb-3">
           <h2 className="text-3xl max-md:text-2xl font-raleway text-center font-semibold">Swift Shopping, <Cover>Speedy Delivery.</Cover></h2>
           <p className="text-xl max-md:text-base px-5 mt-1 font-open_sans max-w-[720px] text-center text-[#17153B]">{heroDescription}</p>
         </div>
